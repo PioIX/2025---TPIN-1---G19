1038a9e83c2973420f3e69b60ef19cfed584b062
bautistamh <bmalagreca@pioix.edu.ar>
2025-07-16 08:01:50 -0300

    Revert "ndad"

--- front/js/dom.js
+++ front/js/dom.js
@@ -24,6 +24,7 @@ class UserInterface {
     }
 
     clearLoginInputs() {
+        document.getElementById("email").value = "";
         document.getElementById("password").value = "";
         document.getElementById("username").value = "";
     }
